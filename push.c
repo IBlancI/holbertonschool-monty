22f92fdcca9e3920454d1608f155a7b65e866dfd
othman93290 <7566@holbertonstudents.com>
2024-01-04 08:36:19 -0800

    push.c

---
+++ push.c
@@ -0,0 +1,110 @@
+#include "monty.h"
+
+/**
+ * f_push - ajoute un nœud à la pile
+ * @head: pointeur vers le sommet de la pile
+ * @counter: numéro de ligne
+ * Return: pas de retour
+ */
+void f_push(stack_t **head, unsigned int counter)
+{
+	int n, j = 0, flag = 0;
+
+	// Vérification si bus.arg est défini
+	if (bus.arg)
+	{
+		// Vérification du signe "-" pour les entiers négatifs
+		if (bus.arg[0] == '-')
+			j++;
+
+		// Vérification de chaque caractère pour s'assurer qu'il s'agit d'un chiffre
+		for (; bus.arg[j] != '\0'; j++)
+		{
+			if (bus.arg[j] > 57 || bus.arg[j] < 48)
+				flag = 1; // Élève le drapeau si le caractère n'est pas un chiffre
+		}
+
+		// Si le drapeau est levé, affiche un message d'erreur et quitte le programme
+		if (flag == 1)
+		{
+			fprintf(stderr, "L%d: usage: push integer\n", counter);
+			fclose(bus.file);
+			free(bus.content);
+			free_stack(*head);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		// Si bus.arg n'est pas défini, affiche un message d'erreur et quitte le programme
+		fprintf(stderr, "L%d: usage: push integer\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+
+	// Conversion de bus.arg en entier avec atoi()
+	n = atoi(bus.arg);
+
+	// Ajout du nœud à la pile ou à la file en fonction de bus.lifi
+	if (bus.lifi == 0)
+		addnode(head, n); // Ajoute un nœud à la pile
+	else
+		addqueue(head, n); // Ajoute un nœud à la file
+}
+#include "monty.h"
+
+/**
+ * f_push - ajoute un nœud à la pile
+ * @head: pointeur vers le sommet de la pile
+ * @counter: numéro de ligne
+ * Return: pas de retour
+ */
+void f_push(stack_t **head, unsigned int counter)
+{
+	int n, j = 0, flag = 0;
+
+	// Vérification si bus.arg est défini
+	if (bus.arg)
+	{
+		// Vérification du signe "-" pour les entiers négatifs
+		if (bus.arg[0] == '-')
+			j++;
+
+		// Vérification de chaque caractère pour s'assurer qu'il s'agit d'un chiffre
+		for (; bus.arg[j] != '\0'; j++)
+		{
+			if (bus.arg[j] > 57 || bus.arg[j] < 48)
+				flag = 1; // Élève le drapeau si le caractère n'est pas un chiffre
+		}
+
+		// Si le drapeau est levé, affiche un message d'erreur et quitte le programme
+		if (flag == 1)
+		{
+			fprintf(stderr, "L%d: usage: push integer\n", counter);
+			fclose(bus.file);
+			free(bus.content);
+			free_stack(*head);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		// Si bus.arg n'est pas défini, affiche un message d'erreur et quitte le programme
+		fprintf(stderr, "L%d: usage: push integer\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+
+	// Conversion de bus.arg en entier avec atoi()
+	n = atoi(bus.arg);
+
+	// Ajout du nœud à la pile ou à la file en fonction de bus.lifi
+	if (bus.lifi == 0)
+		addnode(head, n); // Ajoute un nœud à la pile
+	else
+		addqueue(head, n); // Ajoute un nœud à la file
+}
