1a8e9b2e548eed650aa65caea4988d1d66bd11c0
othman93290 <7566@holbertonstudents.com>
2024-01-05 09:25:30 -0800

    commentary

--- main.c
+++ main.c
@@ -14,32 +14,53 @@ int main(int argc, char *argv[])
 	ssize_t read_line = 1;
 	stack_t *stack = NULL;
 	unsigned int counter = 0;
+        if (argc != 2)
+        {
+                /* Affichage d'un message d'erreur sur la sortie d'erreur standard */
+                fprintf(stderr, "USAGE: monty file\n");
+                /* Sortie du programme avec un code d'erreur */
+                exit(EXIT_FAILURE);
+        }
 
-	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
-	file = fopen(argv[1], "r");
-	bus.file = file;
-	if (!file)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-	while (read_line > 0)
-	{
-		content = NULL;
-		read_line = getline(&content, &size, file);
-		bus.content = content;
-		counter++;
-		if (read_line > 0)
-		{
-			execute(content, &stack, counter, file);
-		}
-		free(content);
-	}
-	free_stack(stack);
-	fclose(file);
-	return (0);
+        /* Ouverture du fichier Monty en lecture */
+        file = fopen(argv[1], "r");
+        /* Stockage du pointeur vers le fichier dans la structure bus */
+        bus.file = file;
+
+        /* Vérification si le fichier s'est bien ouvert */
+        if (!file)
+        {
+                /* Affichage d'un message d'erreur si l'ouverture a échoué */
+                fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+                /* Sortie du programme avec un code d'erreur */
+                exit(EXIT_FAILURE);
+        }
+
+        /* Boucle pour lire chaque ligne du fichier Monty */
+        while (read_line > 0)
+        {
+                /* Initialisation du pointeur de contenu à NULL */
+                content = NULL;
+                /* Lecture d'une ligne du fichier dans la variable content */
+                read_line = getline(&content, &size, file);
+                /* Stockage du contenu de la ligne lue dans la structure bus */
+                bus.content = content;
+                /* Incrémentation du compteur de lignes */
+                counter++;
+
+                /* Vérification si une ligne a été lue avec succès */
+                if (read_line > 0)
+                {
+                        /* Exécution de la commande Monty pour la ligne lue */
+                        execute(content, &stack, counter, file);
+                }
+                /* Libération de la mémoire allouée pour stocker le contenu de la ligne */
+                free(content);
+        }
+        /* Libération de la mémoire utilisée par la pile */
+        free_stack(stack);
+        /* fermeture du fichier Monty */
+        fclose(file);
+
+        return (0);
 }
--- push.c
+++ push.c
@@ -7,31 +7,48 @@
  */
 void f_push(stack_t **head, unsigned int counter)
 {
-	int n, j = 0, flag = 0;
+        int n, j = 0, flag = 0;
 
-	if (bus.arg)
-	{
-		if (bus.arg[0] == '-')
-			j++;
-		for (; bus.arg[j] != '\0'; j++)
-		{
-			if (bus.arg[j] > 57 || bus.arg[j] < 48)
-				flag = 1; }
-		if (flag == 1)
-		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
-			fclose(bus.file);
-			free(bus.content);
-			free_stack(*head);
-			exit(EXIT_FAILURE); }}
-	else
-	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE); }
-	n = atoi(bus.arg);
-	if (bus.lifi == 0)
-		addnode(head, n);
-	else
-		addqueue(head, n);
-}
+        /* Vérification si bus.arg est défini */
+        if (bus.arg)
+        {
+                /* Vérification du signe "-" pour les entiers négatifs */
+                if (bus.arg[0] == '-')
+                        j++;
+
+                /* Vérifie chaque caractère pour s'assurer qu'il s'agit d'un chiffre */
+                for (; bus.arg[j] != '\0'; j++)
+                {
+                        if (bus.arg[j] > 57 || bus.arg[j] < 48)
+                                /* Élève le drapeau si le caractère n'est pas un chiffre */
+                                flag = 1;
+                }
+
+                /* Si le drapeau est levé, quitte et affiche un message */
+                if (flag == 1)
+                {
+                        fprintf(stderr, "L%d: usage: push integer\n", counter);
+                        fclose(bus.file);
+                        free(bus.content);
+                        free_stack(*head);
+                        exit(EXIT_FAILURE);
+                }
+        }
+        else
+        {
+                /* Si bus.arg n'est pas défini, quitte et affiche message*/
+                fprintf(stderr, "L%d: usage: push integer\n", counter);
+                fclose(bus.file);
+                free(bus.content);
+                free_stack(*head);
+                exit(EXIT_FAILURE);
+        }
+
+        /* Conversion de bus.arg en entier avec atoi() */
+        n = atoi(bus.arg);
+
+        /* Ajout du nœud à la pile ou à la file en fonction de bus.lifi */
+        if (bus.lifi == 0)
+                addnode(head, n); /* Ajoute un nœud à la pile */
+        else
+                addqueue(head, n); /* Ajoute un nœud à la f}
