9726daa2e52b3615df0487b182680eebf1997a15
othman93290 <7566@holbertonstudents.com>
2024-01-08 01:46:46 -0800

    commentaire

--- addqueue.c
+++ addqueue.c
@@ -1,59 +1,59 @@
 #include "monty.h"
 
 /**
- * f_queue - prints the top
- * @head: stack head
- * @counter: line_number
- * Return: no return
+ * f_queue - Changes the mode of execution to queue (FIFO)
+ * @head: Pointer to the head of the stack
+ * @counter: Line number
+ * Return: No return value
  */
 void f_queue(stack_t **head, unsigned int counter)
 {
-	// Indique que le mode d'exécution est "queue" (file)
-	(void)head; // Ignorer le paramètre non utilisé
-	(void)counter; // Ignorer le paramètre non utilisé
-	bus.lifi = 1; // Modifier le mode d'exécution pour utiliser une file (queue)
+    /* Change the mode of execution to queue (FIFO) */
+    (void)head; /* Ignoring the unused parameter */
+    (void)counter; /* Ignoring the unused parameter */
+    bus.lifi = 1; /* Setting the mode of execution to use a queue */
 }
 
 /**
- * addqueue - add node to the tail stack
- * @n: new_value
- * @head: head of the stack
- * Return: no return
+ * addqueue - Adds a node to the tail of the stack (queue)
+ * @n: New value to be added
+ * @head: Pointer to the head of the stack
+ * Return: No return value
  */
 void addqueue(stack_t **head, int n)
 {
-	stack_t *new_node, *aux;
+    stack_t *new_node, *aux;
 
-	aux = *head; // Pointeur auxiliaire pour parcourir la pile
+    aux = *head; /* Auxiliary pointer to traverse the stack */
 
-	// Alloue de la mémoire pour un nouveau nœud de pile
-	new_node = malloc(sizeof(stack_t));
-	if (new_node == NULL)
-	{
-		printf("Error\n"); // Affiche une erreur si l'allocation échoue
-	}
+    /* Allocate memory for a new stack node */
+    new_node = malloc(sizeof(stack_t));
+    if (new_node == NULL)
+    {
+        printf("Error\n"); /* Print an error if allocation fails */
+    }
 
-	// Initialise la valeur du nouveau nœud avec la valeur donnée
-	new_node->n = n;
-	new_node->next = NULL;
+    /* Initialize the value of the new node with the given value */
+    new_node->n = n;
+    new_node->next = NULL;
 
-	// Si la pile n'est pas vide, parcourt jusqu'à la fin de la pile
-	if (aux)
-	{
-		while (aux->next)
-			aux = aux->next;
-	}
+    /* If the stack is not empty, traverse to the end of the stack */
+    if (aux)
+    {
+        while (aux->next)
+            aux = aux->next;
+    }
 
-	// Si la pile est vide, le nouveau nœud devient la tête de la pile
-	if (!aux)
-	{
-		*head = new_node;
-		new_node->prev = NULL;
-	}
-	else // Sinon, ajoute le nouveau nœud à la fin de la pile
-	{
-		aux->next = new_node;
-		new_node->prev = aux;
-	}
+    /* If the stack is empty, the new node becomes the head of the stack */
+    if (!aux)
+    {
+        *head = new_node;
+        new_node->prev = NULL;
+    }
+    else /* Otherwise, add the new node to the end of the stack */
+    {
+        aux->next = new_node;
+        new_node->prev = aux;
+    }
 }
 
