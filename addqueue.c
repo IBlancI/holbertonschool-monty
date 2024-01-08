00f76ede136c2e66b934d5a3051de3d205e4f109
othman93290 <7566@holbertonstudents.com>
2024-01-08 01:44:01 -0800

    commentary

--- addqueue.c
+++ addqueue.c
@@ -1,4 +1,5 @@
 #include "monty.h"
+
 /**
  * f_queue - prints the top
  * @head: stack head
@@ -7,9 +8,10 @@
  */
 void f_queue(stack_t **head, unsigned int counter)
 {
-	(void)head;
-	(void)counter;
-	bus.lifi = 1;
+	// Indique que le mode d'exécution est "queue" (file)
+	(void)head; // Ignorer le paramètre non utilisé
+	(void)counter; // Ignorer le paramètre non utilisé
+	bus.lifi = 1; // Modifier le mode d'exécution pour utiliser une file (queue)
 }
 
 /**
@@ -22,27 +24,36 @@ void addqueue(stack_t **head, int n)
 {
 	stack_t *new_node, *aux;
 
-	aux = *head;
+	aux = *head; // Pointeur auxiliaire pour parcourir la pile
+
+	// Alloue de la mémoire pour un nouveau nœud de pile
 	new_node = malloc(sizeof(stack_t));
 	if (new_node == NULL)
 	{
-		printf("Error\n");
+		printf("Error\n"); // Affiche une erreur si l'allocation échoue
 	}
+
+	// Initialise la valeur du nouveau nœud avec la valeur donnée
 	new_node->n = n;
 	new_node->next = NULL;
+
+	// Si la pile n'est pas vide, parcourt jusqu'à la fin de la pile
 	if (aux)
 	{
 		while (aux->next)
 			aux = aux->next;
 	}
+
+	// Si la pile est vide, le nouveau nœud devient la tête de la pile
 	if (!aux)
 	{
 		*head = new_node;
 		new_node->prev = NULL;
 	}
-	else
+	else // Sinon, ajoute le nouveau nœud à la fin de la pile
 	{
 		aux->next = new_node;
 		new_node->prev = aux;
 	}
 }
+
